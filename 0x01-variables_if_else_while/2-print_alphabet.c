63ef72d2c212889004a4310df1a0ce082f187c9c
ERMIYAS TILAHUN <ermy143@gmail.com>
2022-07-01 13:03:34 -0700

    C var 2 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int ch;
 	ch = 'a';
-
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch += 1;
 	}
-	putchar('\n');
-	
+	putchar('\n');	
 	return (0);
 }
